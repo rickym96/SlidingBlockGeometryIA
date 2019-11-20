d04ba9632bd407c878bf0407b0be1262bd1dee21
ricky <ricky@RMF-6296-MK2>
2019-11-20 12:06:04 +0100

    primo

---
+++ SlidingBlockGeometryIA/src/main/java/it/unical/ai/SlidingBlockGeometryIA/App.java
@@ -0,0 +1,13 @@
+package it.unical.ai.SlidingBlockGeometryIA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
